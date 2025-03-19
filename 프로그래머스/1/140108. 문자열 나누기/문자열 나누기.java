8842e87e2e20fb6a99c00ab20d7b81c276e42169
mixedsider <dmstjd0812@gmail.com>
2025-03-19 11:00:12 +0900

    [level 1] Title: 문자열 나누기, Time: 10.36 ms, Memory: 85.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int solution(String s) {
+        String[] sArrays = s.split("");
+        int result = 0;
+        
+        int first = 1;
+        int other = 0;
+        
+        int firstIndex = 0;
+        int otherIndex = 0;
+        
+        for( int i = 1; i < sArrays.length; i++ ) {
+            if( sArrays[firstIndex].equals(sArrays[i]) ) {
+                first++;
+            } else {
+                other++;
+                otherIndex = i;
+            }
+            if( first == other ) {
+                result++;
+                first = 0;
+                other = 0;
+                firstIndex = otherIndex + 1;
+            }
+        }
+        if( sArrays.length != firstIndex) {
+            result ++;
+        }
+        return result;
+    }
+}
