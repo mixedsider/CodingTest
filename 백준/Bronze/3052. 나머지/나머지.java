208e5f813413c12d2e61bee76e127d43f559676b
mixedsider <dmstjd0812@gmail.com>
2025-07-17 19:13:54 +0900

    [Bronze II] Title: 나머지, Time: 168 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    Set<Integer> set = new HashSet<>();
+
+    for( int i = 0; i < 10; i++ ) {
+      int num = sc.nextInt();
+      set.add(num%42);
+    }
+
+    System.out.println(set.size());
+
+  }
+}
